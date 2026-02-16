8fdbfd002b010465146168483f2cdf9801f1c76b
Mart0GD <marty0545@gmail.com>
2026-02-16 15:35:05 +0200

    week_01_added_examples

--- week_01/Lab/Preprocessor_directives.hpp
+++
@@ -1,7 +0,0 @@
-
-/*
-*   Preprocessor directives are lines of code which begin with the # sign. They are resolved 
-*   before actual compilation is done by the preprocessor, from where the name comes.
-*
-*   Preprocessor directives 
-*/
